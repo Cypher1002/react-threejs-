8cc98291929731f0fff7a68fa35006fa0999153a
cypher1002 <mehuldarshni42@gmail.com>
2023-06-04 12:00:07 +0530

    fix

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,5 @@
 import React, {Suspense} from 'react'
-import {Canvas} from '@react-three/fiber'
+import {Canvas} from '@react-three/fiber';
 import {
   Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei"
   import CanvasLoader from '../Loader'
