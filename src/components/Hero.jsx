d1a6f0f7580cb7fa9d727f920980327b02b22d1c
cypher1002 <mehuldarshni42@gmail.com>
2023-06-10 12:02:24 +0530

    fixex

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import {avatar} from "../assets";
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-     <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-5xl mx-auto flex flex-row items-start`}>
+     <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-3npmxl mx-auto flex flex-row items-start`}>
 
        <div className='flex flex-col justify-center items-center mt-5'>
           <div className="w-5 h-5 rounded-full bg-[#84ffc9]"/>     
